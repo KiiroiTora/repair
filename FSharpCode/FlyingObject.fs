418ca39d88f1ca174ab161a0c9ee2821fd4e1f8d
RaduMirceaAndrei00 <mirad28@yahoo.com>
2020-02-16 16:34:19 +0200

    Flying Object written in f#

--- FSharpCode/FlyingObject.fs
+++ FSharpCode/FlyingObject.fs
@@ -7,31 +7,32 @@ type FlyingObjectFs() =
     inherit KinematicBody2D()
     member val obj_type = ObjectType.H
 
-    let sprite = this.GetNode'<Sprite> "Sprite" // ?
+    let sprite = this.GetNode'<Sprite> "Sprite" 
     let area = this.GetNode'<Area2D> "Area2D"
 
     let type = "LH"
     let flying_curve = ResourceLoader.Load'<Curve>("res://Resources/FlyingObjectCurve.tres")
 
-    let rot_speed = RandRange(360*4, 360*8)  // ?
-    let duration = RandRange(360*4, 360*8)  // ?
+    let rot_speed = GD.RandRange(360.0*4.0, 360.0*8.0) 
+    let duration = GD.RandRange(360.0*4.0, 360.0*8.0)  
 
-    signal PickUp(type) // ?
+    [<Signal>]
+    PickUp(type) 
 
-    let mutable velocity = Vector2.Zero // ?
+    let mutable velocity = Vector2.Zero 
     let mutable elapsed = 0.0f
     let can_pick_up = true 
 
-    let mutable particles = Particles2D // ?
+    let mutable particles = Particles2D 
 
     override this._Ready() =
-        velocity <- Vector2(RandRange(-1, 1), RandRange(-1, 1)).Normalized() * RandRange(600, 900)
+        velocity <- Vector2(GD.RandRange(-1.0, 1.0), GD.RandRange(-1.0, 1.0)).Normalized() * GD.RandRange(600.0, 900.0)
 
     override this._Process (delta) =
         elapsed <- elapsed + delta
     	if not (particles = null) then do 
     		GD.Print("Know blood")
-    		particles.GlobalPosition <- (this.GetNode'<Sprite> "Sprite/BloodParticles").GlobalPosition // ?
+    		particles.GlobalPosition <- (this.GetNode'<Sprite> "Sprite/BloodParticles").GlobalPosition 
     		particles.GlobalRotation <- (this.GetNode'<Sprite> "Sprite/BloodParticles").GlobalRotation
 
     	if (elapsed >= duration) then do
@@ -40,8 +41,8 @@ type FlyingObjectFs() =
     	else
     		let coll = MoveAndCollide <| velocity*delta*flying_curve.Interpolate(elapsed/duration)
     		if not (coll = null) do
-    			sprite.Rotate(Deg2Rad(rot_speed) * delta * flying_curve.Interpolate(elapsed/duration)) // ?
-    	area.SetDeferred("monitorable", velocity == Vector2.ZERO) // ?
+    			sprite.Rotate(Deg2Rad(rot_speed) * delta * flying_curve.Interpolate(elapsed/duration)) 
+    	area.SetDeferred("monitorable", velocity == Vector2.ZERO) 
 
 
 
