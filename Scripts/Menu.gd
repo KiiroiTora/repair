2f6456cde17add8792894cdd09a9d2612a21e848
Ivan-Tigan <ivantsoninski@gmail.com>
2020-02-22 16:58:29 +0000

    server starts automatically

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -15,7 +15,7 @@ const quit_y := 593.583
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	anim.play("start")
 	axe_cursor.position.x = start_x
 	axe_cursor.position.y = start_y
 
